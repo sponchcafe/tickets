06e2423dcb32a46e4d4d62f8e10c1b7ed4870e04
Till Zickmantel <till.zickmantel@gmail.com>
2022-07-30 02:48:37 +0200

    _ADD: Ticket list

--- backend/tickets/src/main.rs
+++ backend/tickets/src/main.rs
@@ -3,8 +3,25 @@
 use rocket::http::Header;
 use rocket::{Request, Response};
 use rocket::fairing::{Fairing, Info, Kind};
+use rocket::serde::{Serialize, json};
 
 pub struct CORS;
+
+#[derive(Debug, Serialize)]
+#[serde(crate = "rocket::serde")]
+struct Ticket {
+    title: String,
+    description: String
+}
+
+type Tickets = Vec<Ticket>;
+
+impl Ticket {
+    fn from_text(text: impl AsRef<str>) -> Self {
+        let words: Vec<&str> = text.as_ref().split(" ").collect();
+        Ticket{title: words[0].to_owned(), description: text.as_ref().to_owned()}
+    }
+}
 
 #[rocket::async_trait]
 impl Fairing for CORS {
@@ -29,8 +46,14 @@ fn index() -> &'static str {
 }
 
 #[get("/tickets")]
-fn tickets() -> &'static str {
-    r#"[{"title": "Task 0", "description": "Serve the first ticket"}]"#
+fn tickets() -> json::Json<Tickets> {
+    let tickets: Vec::<Ticket> = [
+        "Work on this task",
+        "And then on this",
+        "Finally work on this",
+    ].into_iter().map(|t| Ticket::from_text(t)).collect();
+    json::Json(tickets)
+    
 }
 
 #[launch]
@@ -38,4 +61,16 @@ fn rocket() -> _ {
     rocket::build()
         .mount("/", routes![index, tickets])
         .attach(CORS)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_ticket_from_text() {
+        let t = Ticket::from_text("Work on this task");
+        assert_eq!(t.title, "Work");
+        assert_eq!(t.description, "Work on this task");
+    }
 }
--- frontend/tickets/src/lib/Ticket.svelte
+++ frontend/tickets/src/lib/Ticket.svelte
@@ -7,3 +7,12 @@
   <h3>{title}</h3>
   <p>{description}</p>
 </div>
+
+<style>
+  div {
+    border: 2px solid #4e4e5e;
+    border-radius: 5px;
+    padding: 5px;
+    margin: 5px;
+  }
+</style>
